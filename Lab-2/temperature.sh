4011596897875299b5b94c36ddcb440851b3ee2a
swethapatil-168 <71492810+swethapatil-168@users.noreply.github.com>
2021-11-11 10:33:11 +0530

    Update temperature.sh

--- Lab-2/temperature.sh
+++ Lab-2/temperature.sh
@@ -2,6 +2,6 @@
 echo "Enter temperature in Fahrenheit"
 read far
 var1=`expr $far - 32`
-var2=`expr 5 / 9`
-c=`expr $var1 \* $var2`
-echo $c
+var2=`expr $var1 \* 5`
+var3=`expr $var2 / 9`
+echo $var3
