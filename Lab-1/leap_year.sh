bfd1f7124d5ec4b26094dd31fee31139a0d2dc5e
swethapatil-168 <71492810+swethapatil-168@users.noreply.github.com>
2021-11-11 23:34:52 +0530

    Update leap_year.sh

--- Lab-1/leap_year.sh
+++ Lab-1/leap_year.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 echo "Enter a year"
 read year
-if test `expr $year % 4` -eq 0 -a `expr $year % 400` -eq 0
+if test `expr $year % 4` -eq 0 -a `expr $year % 100` -ne 0
+then
+ echo "year is a leap year"
+elif test `expr $year % 400` -eq 0
 then
  echo "year is a leap year"
 else 
